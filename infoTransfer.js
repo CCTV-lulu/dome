290dd98d8e9a9d44343a1727cbd576a1bbf4ab52
CCTV-lulu <1253139642@qq.com>
2017-06-14 15:43:42 +0800

    修改整理

--- infoTransfer.js
+++ infoTransfer.js
@@ -4,8 +4,8 @@ var fs = require('fs')
 var unirest = require('unirest')
 var Stream = require('stream').Transform
 
- var parameter = {
-        'keywork':['a','b'] ,
+var parameter = {
+        'keywork':['j','i'] ,
         'pageSize': 5,
         'pageNo': 1,
         'key': '89abb2115a1dc9df29da7f52746486db',
@@ -26,76 +26,61 @@ if(fs.exists('没钱了.txt')){
     option=parameter
 }
 
-    function getJson(option, cb) {
-        console.log('------------------------start---')
-        console.log(option.keyword_index)
-        console.log(option.pageNo)
-        var url = 'http://japi.juhe.cn/trademark/search?keyword='
-            + option.keywork[option.keyword_index] + '&pageSize=' + option.pageSize +
-            '&pageNo=' + option.pageNo + '&key=' + option.key
-        console.log(url)
+function getJson(option, cb) {
+    var url = 'http://japi.juhe.cn/trademark/search?keyword='
+        + option.keywork[option.keyword_index] + '&pageSize=' + option.pageSize +
+        '&pageNo=' + option.pageNo + '&key=' + option.key
 
-        unirest.get(url)
-            .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
-            .send()
-            .end(function (response) {
-                console.log('==========================getresponde')
-                console.log(response)
-                if (response.error) {
-                    writeFile('调用失败', 'keyword:' + option.keywork[option.keyword_index] + ',pageNo:' + option.pageNo)
-                }
-                else {
-                    writeFile('请求', 'keyword:' + option.keywork[option.keyword_index] + ',pageNo:' + option.pageNo)
-                    var newResult = JSON.parse(response.body)
-                    // console.log(typeof newResult.result)
-                    // console.log(newResult.result.data)
-                    // console.log(JSON.stringify(newResult.result.data))
-                    if (newResult.error_code == 0) {
-                        writeFile(option.keywork[option.keyword_index], response.body)
-                        if (newResult.result.data.length === option.pageSize && option.pageNo < 1) {
-                            console.log('=======next page===')
-                            option.pageNo += 1
-                            return getJson(option, cb)
-                        }
-                        if (option.keyword_index<option.keywork.length-1){
-                                console.log('==============next keyword')
-                                option.keyword_index+=1;
-                                option.pageNo=1;
-                                return getJson(option, cb)
-                        }
-                        else {
-                            console.log('=======================cb')
-                            cb()
-                        }
+    unirest.get(url)
+        .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+        .send()
+        .end(function (response) {
+            if (response.error) {
+                writeFile('调用失败', 'keyword:' + option.keywork[option.keyword_index] + ',pageNo:' + option.pageNo)
+            }
+            else {
+                writeFile('请求', 'keyword:' + option.keywork[option.keyword_index] + ',pageNo:' + option.pageNo)
+                var newResult = JSON.parse(response.body)
+                if (newResult.error_code == 0) {
+                    writeFile(option.keywork[option.keyword_index], JSON.stringify(newResult.result.data))
+                    if (newResult.result.data.length === option.pageSize && option.pageNo < 1) {
+                        option.pageNo += 1
+                        return getJson(option, cb)
                     }
-                    else if (newResult.error_code == 10012) {
-                        return coverFile('没钱了', 'keyword:' + option.keywork[option.keyword_index] + ',pageNo:' + option.pageNo)
+                    if (option.keyword_index<option.keywork.length-1){
+                        option.keyword_index+=1;
+                        option.pageNo=1;
+                        return getJson(option, cb)
                     }
-
+                    else {
+                        cb()
+                    }
                 }
-            })
+                else if (newResult.error_code == 10012) {
+                    return coverFile('没钱了', 'keyword:' + option.keywork[option.keyword_index] + ',pageNo:' + option.pageNo)
+                }
+
+            }
+        })
+}
+
+function writeFile(keyword, data) {
+    var txt_info =  readFile(keyword);
+    if (txt_info == null) {
+        fs.appendFileSync('./' + keyword + '.txt', data)
+    } else {
+        fs.appendFileSync('./' + keyword + '.txt', '*' + data)
     }
+}
 
-    function writeFile(keyword, data) {
-        var txt_info =  readFile(keyword);
-
-        if (txt_info == null) {
-            console.log('./' + keyword + '.txt');
-            console.log(data);
-            fs.appendFileSync('./' + keyword + '.txt', data)
-        } else {
-            fs.appendFileSync('./' + keyword + '.txt', '*' + data)
-        }
+function readFile(keyword) {
+    var txt_file = fs.existsSync('./' + keyword + '.txt')
+    if (txt_file == true) {
+        var data = fs.readFileSync('./' + keyword + '.txt',{flag: 'r+', encoding: 'utf8'})
+        return data
+    } else {
+        return null
     }
-
-    function readFile(keyword) {
-        var txt_file = fs.existsSync('./' + keyword + '.txt')
-        if (txt_file == true) {
-            var data = fs.readFileSync('./' + keyword + '.txt')
-            return data.toString()
-        } else {
-            return null
-        }
 
     }
 function coverFile(keyword,data) {
@@ -111,7 +96,7 @@ function coverFile(keyword,data) {
 getJson(option, function () {
     option.keywork.forEach(function (i) {
         if(!fs.existsSync('./'+i)){
-           fs.mkdirSync(i)
+            fs.mkdirSync(i)
         }
         var info =  readFile(i)
         if(info!=null) {
@@ -119,20 +104,22 @@ getJson(option, function () {
             newImg = info.split('*')
             newImg.forEach(function (a) {
                 var datainfo = JSON.parse(a)
-                datainfo.result.data.forEach(function (k) {
-                    var lastimg = 'http://pic.tmkoo.com/pic.php?zch=' + k.tmImg
-                    http.request(lastimg, function (response) {
-                        var data = new Stream();
+                datainfo.forEach(function (k) {
+                    if (k.tmImg!=undefined) {
+                        var lastimg = 'http://pic.tmkoo.com/pic.php?zch=' + k.tmImg
+                        http.request(lastimg, function (response) {
+                            var data = new Stream();
 
-                        response.on('data', function (chunk) {
-                            data.push(chunk)
-                        })
+                            response.on('data', function (chunk) {
+                                data.push(chunk)
+                            })
 
-                        response.on('end', function () {
-                            fs.writeFile('./' + i + '/' + k.tmImg + '.png', data.read())
-                        })
+                            response.on('end', function () {
+                                fs.writeFile('./' + i + '/' + k.tmImg + '.png', data.read())
+                            })
 
-                    }).end()
+                        }).end()
+                    }
                 })
             })
         }
